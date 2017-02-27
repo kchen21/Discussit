ce59ebef8444dda59a13a876b22d855b162d3dae
Kenneth Chen <kennethchendbgt@gmail.com>
2017-02-27 15:47:30 -0500

    Set up Sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,18 @@
 class SessionsController < ApplicationController
+  before_action :require_logged_out, only: [:new, :create]
+  before_action :require_logged_in, only: [:destroy]
+
+  def new
+  end
+
+  def create
+    user = User.find_by_credentials(
+    params[:user][:username],
+    params[:user][:password])
+  end
+
+  def destroy
+    log_out
+    redirect_to new_session_url
+  end
 end
