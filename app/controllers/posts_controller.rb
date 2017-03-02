cdaac8d1172b1d0bbce500f500de366584f5e8d5
Kenneth Chen <kennethchendbgt@gmail.com>
2017-03-01 20:12:30 -0500

    Update Posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
   before_action :require_logged_in, only: [:create, :new, :edit, :update, :destroy]
   before_action :require_current_user_owns_post, only: [:edit, :update]
-  before_action :require_current_user_or_moderator_owns_post, only: [:destroy]
+  before_action :require_current_user_owns_post_or_subforum, only: [:destroy]
 
   private
 
@@ -9,6 +9,15 @@ class PostsController < ApplicationController
     return if current_user.posts.find_by(id: params[:id])
     @post = Post.find(params[:id])
     flash[:errors] = ["You are not the creator of this post"]
+    redirect_to post_url(@post)
+  end
+
+  def require_current_user_owns_post_or_subforum
+    return if (
+    current_user.subforums.find_by(id: params[:subforum_id]) || current_user.posts.find_by(id: params[:id])
+    )
+    @post = Post.find(params[:id])
+    flash[:errors] = ["You are not the post's creator or subforum's moderator"]
     redirect_to post_url(@post)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   resources :users, only: [:new, :create, :edit, :update]
   resource :session, only: [:new, :create, :destroy]
-  resources :subforums
-  resources :posts, except: [:index]
+  resources :subforums do
+    resources :posts, except: [:index]
+  end
 end
