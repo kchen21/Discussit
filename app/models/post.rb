28bf0decf953c85493966c9350986b306df0be75
Kenneth Chen <kennethchendbgt@gmail.com>
2017-03-03 00:33:41 -0500

    Update view to render nested comments

--- app/models/post.rb
+++ app/models/post.rb
@@ -26,4 +26,14 @@ class Post < ApplicationRecord
   )
 
   has_many :comments
+
+  def comments_grouped_by_parent
+    comments_grouped_by_parent = Hash.new { |hash, key| hash[key]= [] }
+
+    self.comments.includes(:author).each do |comment|
+      comments_grouped_by_parent[comment.parent_comment_id)] << comment
+    end
+
+    comments_grouped_by_parent
+  end
 end
