aa3135581bbaaee1e68969c3642340b2fe727174
Kenneth Chen <kennethchendbgt@gmail.com>
2017-03-03 15:16:50 -0500

    Set up Vote model and Votable concern

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,2 +1,20 @@
+# == Schema Information
+#
+# Table name: votes
+#
+#  id           :integer          not null, primary key
+#  value        :integer          not null
+#  user_id      :integer          not null
+#  votable_id   :integer          not null
+#  votable_type :integer          not null
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
+
 class Vote < ApplicationRecord
+  validates :user, presence: true
+  validates :user_id, uniqueness: { scope: [:votable_id, :votable_type] }
+
+  belongs_to :user
+  belongs_to :votable, polymorphic: true
 end
--- test/models/vote_test.rb
+++ test/models/vote_test.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: votes
+#
+#  id           :integer          not null, primary key
+#  value        :integer          not null
+#  user_id      :integer          not null
+#  votable_id   :integer          not null
+#  votable_type :integer          not null
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
+
 require 'test_helper'
 
 class VoteTest < ActiveSupport::TestCase
