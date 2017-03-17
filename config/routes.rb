a881dfc7987d7700bef7252a719e45f4bd463c27
Kenneth Chen <kennethchendbgt@gmail.com>
2017-03-17 00:34:19 -0400

    Create guest user and buttons to log in as guest

--- config/routes.rb
+++ config/routes.rb
@@ -19,5 +19,5 @@ Rails.application.routes.draw do
     end
   end
 
-  root to: redirect("/subforums")
+  root to: redirect("/users/new")
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create!(first_name: 'Guest', last_name: 'K331', email: 'guest@discussit.com', username: 'Guest', password: '123456')
